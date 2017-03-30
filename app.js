9cb46ee39b6c73e62de4435e19022968285b96a6
Carlos Robayo <carlos.robayo@globant.com>
2017-03-30 16:52:38 -0500

    Stage 1

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+var express = require('express');
 
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+
+    if(req.url == '/' || req.url == '/home') {
+        res.sendFile(__dirname + '/public/index.html');
+    } else {
+        res.sendFile(__dirname + '/public/views/404.html');
+    }
+
+});
+
+app.listen(8000, function() {
+    console.log('Esperando peticiones en el puerto 7000');
+});
---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+function printCats() {
+
+    var numberCats = document.getElementById('number-cats').value;
+
+    for (var index = 0; index < numberCats; index++) {
+        document.getElementById('containerCats').innerHTML += 
+        '<div class="container text-center">' +
+            '<img src="img/project/cat.jpg" alt="Image Cat" id="cat" class="cat_image">' +
+            '<br>' +
+            '<span id="contador'+index+'" class="badge badge-primary contador"></span>' +
+        '</div>'
+    }
+
+    var cats = Array.from( document.querySelectorAll('img') ); 
+    var counters = Array.from( document.querySelectorAll('#containerCats span') ); 
+
+    cats.forEach(function(cat, index) {
+
+        // cat.index = 0;
+
+        cat.addEventListener('click', function() {
+            this.index ++;
+            counters[index].innerHTML = this.index;
+        });
+
+        console.log(counters[index]);
+
+    });
+
+}
